fd3ef869b7e267d9c4a5920147f9b083050edb24
tw234-jpg <103188290+tw234-jpg@users.noreply.github.com>
2022-11-03 17:09:09 +0900

    progateで作成

---
+++ js/variable.js
@@ -0,0 +1,43 @@
+// 1
+// 変数nameを定義し、「にんじゃわんこ」を代入してください
+let name = "にんじゃわんこ";
+
+// 変数nameの値を出力してください
+console.log( name );
+
+
+// 2
+// 変数lengthを定義してください
+let length = 5;
+
+// 変数lengthの値を出力してください
+console.log( length );
+
+// 変数lengthを用いて、円の面積を出力してください
+console.log( length * length * 3 );
+
+
+//3
+let Name = "にんじゃわんこ";
+console.log(Name);
+
+// 変数nameの値を"とりずきん"に更新してください
+Name = "とりずきん";
+
+// 変数nameの値を出力してください
+console.log( Name );
+
+
+//4
+let number = 7;
+console.log(number);
+
+// 変数numberの値に3を加えてください
+number += 3;
+
+console.log(number);
+
+// 変数numberの値を2で割ってください
+number /= 2;
+
+console.log(number);
