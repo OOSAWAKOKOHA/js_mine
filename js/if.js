0704f9ccdfd984eded7bc8ea51f116a9de5db660
tw234-jpg <103188290+tw234-jpg@users.noreply.github.com>
2022-11-04 12:23:05 +0900

    ページ追加

---
+++ js/if.js
@@ -0,0 +1,23 @@
+// if文
+const level = 12;
+
+// 条件式を「level > 10」とするif文を作ってください
+if( level > 10 ){
+  console.log("レベルが10より大きいです");
+}
+
+
+// 真偽値と比較演算子（1）
+const age = 24;
+
+// 「age >= 20」を出力してください
+
+console.log( age >= 20 );
+
+// 「age < 20」を出力してください
+console.log( age < 20 );
+
+// ageの値が20以上の場合に、「私は20歳以上です」と出力してください
+if( age >= 20 ){
+  console.log("私は20歳以上です");
+}
